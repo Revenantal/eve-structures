8e8ed58df830837436bc9e99161f6f1182baad95
sydneybucc <s_bucci@fanshaweonline.ca>
2018-03-03 19:20:31 -0500

    adding username to login bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    USERNAME <span class="caret"></span>
+                                    {{ Auth::user()->character_name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
