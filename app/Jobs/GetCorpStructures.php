2c2c74b94961764b157f25d30712c92e46ae1230
revenantal <5674404+revenantal@users.noreply.github.com>
2018-03-22 19:41:56 -0400

    Setting up the basic job system for new users. Will need to setup supervisor

---
+++ app/Jobs/GetCorpStructures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Corporation;
+use App\Models\Structure;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class GetCorpStructures
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $corp;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Corporation $corp)
+    {
+        $this->corp = $corp;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        \Log::info('UPDATING');
+        Structure::UpdateStructures($this->corp);
+        \Log::info('DONE');
+    }
+}
